6ae734ac772e316926871910caf62a29631b67dd
hamzatal <hamza.talllll@gmail.com>
2025-11-15 19:24:00 +0300

    Add Breeze

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    darkMode: "class", 
+export default {
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.jsx",
-        "./resources/**/*.js",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.jsx',
     ],
+
     theme: {
         extend: {
             fontFamily: {
-                sans: ["Montserrat", "sans-serif"],
-            },
-            colors: {
-                // Light Mode
-                primary: "#5F7F3B",
-                secondary: "#D49D2F",
-                background: "#F9F9F9",
-                text: "#333333",
-                textSecondary: "#666666",
-                hover: "#4E6B32",
-                success: "#7BB661",
-                error: "#E94E3C",
-
-                // Dark Mode
-                dark: {
-                    background: "#1F1F1F",
-                    primary: "#5F7F3B",
-                    secondary: "#B8851F",
-                    text: "#F5F5F5",
-                    textSecondary: "#CCCCCC",
-                    hover: "#7BB661",
-                    success: "#A3D18E",
-                    error: "#F5625F",
-                },
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
     },
-    plugins: [],
+
+    plugins: [forms],
 };
